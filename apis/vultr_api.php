976ced9a6440a162cfc363db89e51aab21e3c396
Jonathan Reissmueller <jonathan@phillipsdata.com>
2021-09-24 09:52:39 -0700

    Updated Verify Host on cURL call

--- apis/vultr_api.php
+++ apis/vultr_api.php
@@ -59,7 +59,7 @@ class VultrApi
 
         if (Configure::get('Blesta.curl_verify_ssl')) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         } else {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
